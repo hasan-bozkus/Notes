beb794ef28a0a5a2446c42e994a4cb2d38ce563f
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-12 22:19:27 +0300

    Add project files.

---
+++ Notes/Models/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    internal class About
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "Bu uygulama Hasan Bozkuş tarafından geliştirildi.";
+    }
+}
